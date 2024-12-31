ddb9dce04b2da8c19aa177114d791e7ffd373dff
Elynam <yawahelynam@gmail.com>
2024-12-31 10:30:27 -0600

    created user router

---
+++ server/jsonWebToken.js
@@ -0,0 +1,15 @@
+const jwt = require('jsonwebtoken');
+
+const verifyToken = (req, res, next) => {
+    const token = req.cookies.token;
+
+    jwt.verify(token, SECRET_KEY, {}, async (err, data) => {
+        if(err){
+            res.status(403).json("Token is Invalid!");
+        }
+        req.userId = data._id
+        next()
+    })
+}
+
+module.exports = verifyToken;
