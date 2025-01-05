387e26d3641db09f65bc33ff7324d50e46dff0ae
Elynam <yawahelynam@gmail.com>
2025-01-05 12:01:07 -0600

    creating navbar

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,10 +1,31 @@
-import './App.css'
+import Home from './pages/Home';
+import Login from './pages/Login';
+import Register from './pages/Register';
+import CreatePost from './pages/CreatePost';
+import PostDetails from './pages/PostDetails';
+import EditPost from './pages/EditPost';
+import MyBlogs from './pages/MyBlogs';
+import Profile from './pages/Profile';
+
+import NavBar from './components/NavBar';
+import { Routes, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <Routes>
-      <Route exact path="" element={}/>
-    </Routes>
+    <>
+    <NavBar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
+        <Route path="/write" element={<CreatePost />} />
+        <Route path="/post/post/:id" element={<PostDetails />} />
+        <Route path="/edit/:id" element={<EditPost />} />
+        <Route path="/myblogs/:id" element={<MyBlogs />} />
+        <Route path="/profile/:id" element={<Profile />} />
+        <Route path="*" element={<h1>404: Page Not Found</h1>} />
+      </Routes>
+    </>
   )
 }
 
--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -1,9 +1,22 @@
 import React from 'react'
+import { useState } from 'react';
+import { useNavigate, useLocation } from 'react-router-dom';
 
 const NavBar = () => {
+  const [ prompt, setPrompt ] = useState("");
+  const [ menu, setMenu ] = useState(false);
+  const navigate = useNavigate();
+  const path = useLocation().pathname;
+
   return (
-    <div>
-      
+    <div className='bg-yellow-400 flex'>
+      <h1 className='text-gray-50'>Logo</h1>
+      {
+        path === "/" && <div onChange={(e) => {setPrompt(e.target.value)}} className='flex'>
+          <input type="text" placeholder='Search for a post...' value={prompt}/>
+          <p onClick={() => {navigate(prompt ? '?search' + prompt : navigate("/"))}}>🍳</p>
+        </div>
+      }
     </div>
   )
 }
